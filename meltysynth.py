8bb8f93c959629c61b6533fcb5a15e3693fe30db
田中信秋 <sinshu.d@gmail.com>
2022-08-27 20:33:48 +0900

    Add instrument region.

--- meltysynth.py
+++ meltysynth.py
@@ -1,6 +1,7 @@
 import array
 import io
 import itertools
+import math
 from enum import Enum
 
 
@@ -65,6 +66,62 @@ class BinaryReaderEx:
             result[i] = BinaryReaderEx.read_int16(reader)
         
         return result
+
+
+
+class SoundFontMath:
+
+    @staticmethod
+    def half_pi() -> float:
+        return math.pi / 2
+    
+    @staticmethod
+    def non_audible() -> float:
+        return 1.0E-3
+    
+    @staticmethod
+    def log_non_audible() -> float:
+        return math.log(1.0E-3)
+    
+    @staticmethod
+    def timecents_to_seconds(x: float) -> float:
+        return math.pow(2.0, (1.0 / 1200.0) * x)
+    
+    @staticmethod
+    def cents_to_hertz(x: float) -> float:
+        return 8.176 * math.pow(2.0, (1.0 / 1200.0) * x)
+    
+    @staticmethod
+    def cents_to_multiplying_factor(x: float) -> float:
+        return math.pow(2.0, (1.0 / 1200.0) * x)
+    
+    @staticmethod
+    def decibels_to_linear(x: float) -> float:
+        return math.pow(10.0, 0.05 * x)
+    
+    @staticmethod
+    def linear_to_decibels(x: float) -> float:
+        return 20.0 * math.log10(x)
+    
+    @staticmethod
+    def key_number_to_multiplying_factor(cents: int, key: int) -> float:
+        return SoundFontMath.timecents_to_seconds(cents * (60 - key))
+    
+    @staticmethod
+    def exp_cutoff(x: float) -> float:
+        if x < SoundFontMath.log_non_audible():
+            return 0.0
+        else:
+            return math.exp(x)
+    
+    @staticmethod
+    def clamp(value: float, min: float, max: float) -> float:
+        if value < min:
+            return min
+        elif value > max:
+            return max
+        else:
+            return value
 
 
 
@@ -654,6 +711,315 @@ class InstrumentInfo:
         
         return instruments
 
+    @property
+    def name(self) -> str:
+        return self._name
+    
+    @property
+    def zone_start_index(self) -> int:
+        return self._zone_start_index
+    
+    @property
+    def zone_end_index(self) -> int:
+        return self._zone_end_index
+
+
+
+class Instrument:
+
+    name: str
+
+
+
+class LoopMode(Enum):
+
+    NO_LOOP = 0
+    CONTINUOUS = 1
+    LOOP_UNTIL_NOTE_OFF = 3
+
+
+
+class InstrumentRegion:
+
+    _sample: SampleHeader
+    _gs: array.array
+
+    def __init__(self, instrument: Instrument, global_generators: list, local_generators: list, samples: list) -> None:
+        
+        _gs = array.array("h", itertools.repeat(0, 64))
+        _gs[GeneratorType.INITIAL_FILTER_CUTOFF_FREQUENCY] = 13500
+        _gs[GeneratorType.DELAY_MODULATION_LFO] = -12000
+        _gs[GeneratorType.DELAY_VIBRATO_LFO] = -12000
+        _gs[GeneratorType.DELAY_MODULATION_ENVELOPE] = -12000
+        _gs[GeneratorType.ATTACK_MODULATION_ENVELOPE] = -12000
+        _gs[GeneratorType.HOLD_MODULATION_ENVELOPE] = -12000
+        _gs[GeneratorType.DECAY_MODULATION_ENVELOPE] = -12000
+        _gs[GeneratorType.RELEASE_MODULATION_ENVELOPE] = -12000
+        _gs[GeneratorType.DELAY_VOLUME_ENVELOPE] = -12000
+        _gs[GeneratorType.ATTACK_VOLUME_ENVELOPE] = -12000
+        _gs[GeneratorType.HOLD_VOLUME_ENVELOPE] = -12000
+        _gs[GeneratorType.DECAY_VOLUME_ENVELOPE] = -12000
+        _gs[GeneratorType.RELEASE_VOLUME_ENVELOPE] = -12000
+        _gs[GeneratorType.KEY_RANGE] = 0x7F00
+        _gs[GeneratorType.VELOCITY_RANGE] = 0x7F00
+        _gs[GeneratorType.KEY_NUMBER] = -1
+        _gs[GeneratorType.VELOCITY] = -1
+        _gs[GeneratorType.SCALE_TUNING] = 100
+        _gs[GeneratorType.OVERRIDING_ROOT_KEY] = -1
+
+        if global_generators != None:
+            for parameter in global_generators:
+                self.set_parameter(parameter)
+        
+        if local_generators != None:
+            for parameter in local_generators:
+                self.set_parameter(parameter)
+        
+        id = _gs[GeneratorType.SAMPLE_ID]
+        if not (0 <= id and id < samples.Length):
+            raise Exception("The instrument '" + instrument.name + "' contains an invalid sample ID '" + id + "'.")
+        self._sample = samples[id]
+    
+    @staticmethod
+    def create(instrument: Instrument, zones: list, samples: list):
+
+        global_zone: Zone = None
+
+        # Is the first one the global zone?
+        if len(zones[0].generators) == 0 or zones[0].generators[-1].generator_type != GeneratorType.SampleID:
+            # The first one is the global zone.
+            global_zone = zones[0]
+        
+        if global_zone != None:
+            # The global zone is regarded as the base setting of subsequent zones.
+            count = len(zones) - 1
+            regions = list()
+            for i in range(count):
+                regions.append(InstrumentRegion(instrument, global_zone.generators, zones[i + 1].generators, samples))
+            return regions
+        else:
+            # No global zone.
+            count = len(zones)
+            regions = list()
+            for i in range(count):
+                regions.append(InstrumentRegion(instrument, None, zones[i].generators, samples))
+            return regions
+
+    def set_parameter(self, parameter: Generator):
+
+        index = int(parameter.generator_type)
+
+        # Unknown generators should be ignored.
+        if 0 <= index and index < len(self._gs):
+            self._gs[index] = parameter.value
+    
+    def contains(self, key: int, velocity: int):
+        contains_key = self.key_range_start <= key and key <= self.key_range_end
+        contains_velocity = self.velocity_range_start <= velocity and velocity <= self.velocity_range_end
+        return contains_key and contains_velocity
+
+    @property
+    def sample_start(self) -> int:
+        return self._sample.start + self.start_address_offset
+
+    @property
+    def sample_end(self) -> int:
+        return self._sample.end + self.end_address_offset
+
+    @property
+    def sample_start_loop(self) -> int:
+        return self._sample.start_loop + self.start_loop_address_offset
+
+    @property
+    def sample_end_loop(self) -> int:
+        return self._sample.end_loop + self.end_loop_address_offset
+
+    @property
+    def start_address_offset(self) -> int:
+        return 32768 * self._gs[GeneratorType.START_ADDRESS_COARSE_OFFSET] + self._gs[GeneratorType.START_ADDRESS_OFFSET]
+
+    @property
+    def end_address_offset(self) -> int:
+        return 32768 * self._gs[GeneratorType.END_ADDRESS_COARSE_OFFSET] + self._gs[GeneratorType.END_ADDRESS_OFFSET]
+
+    @property
+    def start_loop_address_offset(self) -> int:
+        return 32768 * self._gs[GeneratorType.START_LOOP_ADDRESS_COARSE_OFFSET] + self._gs[GeneratorType.START_LOOP_ADDRESS_OFFSET]
+
+    @property
+    def end_loop_address_offset(self) -> int:
+        return 32768 * self._gs[GeneratorType.END_LOOP_ADDRESS_COARSE_OFFSET] + self._gs[GeneratorType.END_LOOP_ADDRESS_OFFSET]
+
+    @property
+    def modulation_lfo_to_pitch(self) -> int:
+        return self._gs[GeneratorType.MODULATION_LFO_TO_PITCH]
+
+    @property
+    def vibrato_lfo_to_pitch(self) -> int:
+        return self._gs[GeneratorType.VIBRATO_LFO_TO_PITCH]
+
+    @property
+    def modulation_envelope_to_pitch(self) -> int:
+        return self._gs[GeneratorType.MODULATION_ENVELOPE_TO_PITCH]
+
+    @property
+    def initial_filter_cutoff_frequency(self) -> float:
+        return SoundFontMath.cents_to_hertz(self._gs[GeneratorType.INITIAL_FILTER_CUTOFF_FREQUENCY])
+
+    @property
+    def initial_filter_q(self) -> float:
+        return 0.1 * self._gs[GeneratorType.INITIAL_FILTER_Q]
+
+    @property
+    def modulation_lfo_to_filter_cutoff_frequency(self) -> int:
+        return self._gs[GeneratorType.MODULATION_LFO_TO_FILTER_CUTOFF_FREQUENCY]
+
+    @property
+    def modulation_envelope_to_filter_cutoff_frequency(self) -> int:
+        return self._gs[GeneratorType.MODULATION_ENVELOPE_TO_FILTER_CUTOFF_FREQUENCY]
+
+    @property
+    def modulation_lfo_to_volume(self) -> float:
+        return 0.1 * self._gs[GeneratorType.MODULATION_LFO_TO_VOLUME]
+
+    @property
+    def chorus_effects_send(self) -> float:
+        return 0.1 * self._gs[GeneratorType.CHORUS_EFFECTS_SEND]
+
+    @property
+    def reverb_effects_send(self) -> float:
+        return 0.1 * self._gs[GeneratorType.REVERB_EFFECTS_SEND]
+
+    @property
+    def pan(self) -> float:
+        return 0.1 * self._gs[GeneratorType.PAN]
+
+    @property
+    def delay_modulation_lfo(self) -> float:
+        return SoundFontMath.timecents_to_seconds(self._gs[GeneratorType.DELAY_MODULATION_LFO])
+
+    @property
+    def frequency_modulation_lfo(self) -> float:
+        return SoundFontMath.cents_to_hertz(self._gs[GeneratorType.FREQUENCY_MODULATION_LFO])
+
+    @property
+    def delay_vibrato_lfo(self) -> float:
+        return SoundFontMath.timecents_to_seconds(self._gs[GeneratorType.DELAY_VIBRATO_LFO])
+
+    @property
+    def frequency_vibrato_lfo(self) -> float:
+        return SoundFontMath.cents_to_hertz(self._gs[GeneratorType.FREQUENCY_VIBRATO_LFO])
+
+    @property
+    def delay_modulation_envelope(self) -> float:
+        return SoundFontMath.timecents_to_seconds(self._gs[GeneratorType.DELAY_MODULATION_ENVELOPE])
+
+    @property
+    def attack_modulation_envelope(self) -> float:
+        return SoundFontMath.timecents_to_seconds(self._gs[GeneratorType.ATTACK_MODULATION_ENVELOPE])
+
+    @property
+    def hold_modulation_envelope(self) -> float:
+        return SoundFontMath.timecents_to_seconds(self._gs[GeneratorType.HOLD_MODULATION_ENVELOPE])
+
+    @property
+    def decay_modulation_envelope(self) -> float:
+        return SoundFontMath.timecents_to_seconds(self._gs[GeneratorType.DECAY_MODULATION_ENVELOPE])
+
+    @property
+    def sustain_modulation_envelope(self) -> float:
+        return 0.1 * self._gs[GeneratorType.SUSTAIN_MODULATION_ENVELOPE]
+
+    @property
+    def release_modulation_envelope(self) -> float:
+        return SoundFontMath.timecents_to_seconds(self._gs[GeneratorType.RELEASE_MODULATION_ENVELOPE])
+
+    @property
+    def key_number_to_modulation_envelope_hold(self) -> int:
+        return self._gs[GeneratorType.KEY_NUMBER_TO_MODULATION_ENVELOPE_HOLD]
+
+    @property
+    def key_number_to_modulation_envelope_decay(self) -> int:
+        return self._gs[GeneratorType.KEY_NUMBER_TO_MODULATION_ENVELOPE_DECAY]
+
+    @property
+    def delay_volume_envelope(self) -> float:
+        return SoundFontMath.timecents_to_seconds(self._gs[GeneratorType.DELAY_VOLUME_ENVELOPE])
+
+    @property
+    def attack_volume_envelope(self) -> float:
+        return SoundFontMath.timecents_to_seconds(self._gs[GeneratorType.ATTACK_VOLUME_ENVELOPE])
+
+    @property
+    def hold_volume_envelope(self) -> float:
+        return SoundFontMath.timecents_to_seconds(self._gs[GeneratorType.HOLD_VOLUME_ENVELOPE])
+
+    @property
+    def decay_volume_envelope(self) -> float:
+        return SoundFontMath.timecents_to_seconds(self._gs[GeneratorType.DECAY_VOLUME_ENVELOPE])
+
+    @property
+    def sustain_volume_envelope(self) -> float:
+        return 0.1 * self._gs[GeneratorType.SUSTAIN_VOLUME_ENVELOPE]
+
+    @property
+    def release_volume_envelope(self) -> float:
+        return SoundFontMath.timecents_to_seconds(self._gs[GeneratorType.RELEASE_VOLUME_ENVELOPE])
+
+    @property
+    def key_number_to_volume_envelope_hold(self) -> int:
+        return self._gs[GeneratorType.KEY_NUMBER_TO_VOLUME_ENVELOPE_HOLD]
+
+    @property
+    def key_number_to_volume_envelope_decay(self) -> int:
+        return self._gs[GeneratorType.KEY_NUMBER_TO_VOLUME_ENVELOPE_DECAY]
+
+    @property
+    def key_range_start(self) -> int:
+        return self._gs[GeneratorType.KEY_RANGE] & 0xFF
+
+    @property
+    def key_range_end(self) -> int:
+        return (self._gs[GeneratorType.KEY_RANGE] >> 8) & 0xFF
+
+    @property
+    def velocity_range_start(self) -> int:
+        return self._gs[GeneratorType.VELOCITY_RANGE] & 0xFF
+
+    @property
+    def velocity_range_end(self) -> int:
+        return (self._gs[GeneratorType.VELOCITY_RANGE] >> 8) & 0xFF
+
+    @property
+    def initial_attenuation(self) -> float:
+        return 0.1 * self._gs[GeneratorType.INITIAL_ATTENUATION]
+
+    @property
+    def coarse_tune(self) -> int:
+        return self._gs[GeneratorType.COARSE_TUNE]
+
+    @property
+    def fine_tune(self) -> int:
+        return self._gs[GeneratorType.FINE_TUNE] + self._sample.pitch_correction
+
+    @property
+    def sample_modes(self) -> LoopMode:
+        return LoopMode(self._gs[GeneratorType.SAMPLE_MODES]) if self._gs[GeneratorType.SAMPLE_MODES] != 2 else LoopMode.NO_LOOP
+
+    @property
+    def scale_tuning(self) -> int:
+        return self._gs[GeneratorType.SCALE_TUNING]
+
+    @property
+    def exclusive_class(self) -> int:
+        return self._gs[GeneratorType.EXCLUSIVE_CLASS]
+
+    @property
+    def root_key(self) -> int:
+        return self._gs[GeneratorType.OVERRIDING_ROOT_KEY] if self._gs[GeneratorType.OVERRIDING_ROOT_KEY] != -1 else self._sample.original_pitch
+
+
+
 
 
 class SoundFontParameters:
@@ -680,6 +1046,7 @@ class SoundFontParameters:
         instrument_infos: list = None
         instrument_bag: list = None
         instrument_generators: list = None
+        sample_headers: list = None
 
         while reader.tell() < end:
 
